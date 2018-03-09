e3600dab3e0035c933f0561ec953bdd4209ea00a
Sara Tibbetts <sxt4866@rit.edu>
2018-03-09 15:26:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :content, length: { minimum: 250 }
-  validates 
+  validates :summary, length: { maximum: 250 } 
 end
