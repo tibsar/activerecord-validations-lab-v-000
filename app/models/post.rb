3937d8436e36acc19a8530e0400b923db97b18d9
Sara Tibbetts <sxt4866@rit.edu>
2018-03-09 15:38:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 } 
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  
 end
 
 VALID_CLICKBAIT = [
